37a86f7fcb13bcf96c5862000674714f45b94204
Eduardo Shanahan <eduardo.shanahan@arrowheadmind.com>
2026-02-23 16:02:18 +0000

    network: prevent DHCP transient hostname override

--- nixos/modules/network.nix
+++ nixos/modules/network.nix
@@ -3,4 +3,10 @@
   networking.useDHCP = true;
   networking.domain = config.lab.domain;
   networking.wireless.enable = false;
+
+  # Keep declarative hostnames (networking.hostName) authoritative.
+  # Without this, DHCP can set a transient hostname (for example "nixos").
+  networking.dhcpcd.extraConfig = ''
+    nohook hostname
+  '';
 }
