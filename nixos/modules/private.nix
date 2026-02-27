05f2e8da401d8710b8308d9551257e3f0c112161
Eduardo Shanahan <eduardo.shanahan@arrowheadmind.com>
2026-02-27 19:31:28 +0000

    rebuild

--- nixos/modules/private.nix
+++ nixos/modules/private.nix
@@ -7,4 +7,10 @@ in
     if builtins.pathExists privateOverrides
     then [ privateOverrides ]
     else [];
+
+  # Trust the internal wildcard certificate on all lab hosts so local HTTPS
+  # clients can verify services served behind Traefik.
+  security.pki.certificateFiles = [
+    ../certs/hhlab-wildcard.crt
+  ];
 }
